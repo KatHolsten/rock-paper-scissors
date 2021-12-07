5455d1075c807992d455d211de329e1012adb42a
Kat <katholsten@gmail.com>
2021-12-07 12:18:34 -0800

    changed functions to fit assignment, playRound function is currently broken

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-//user enters a choice into a prompt
-let userChoice = prompt('What do you choose? Rock, Paper or Scissors?').toLowerCase();
-console.log('User choice: ' + userChoice);
-    
-//computer randomly generates a choice and compares it to userChoice
-   function computerPlay() {
+
+//The computer makes a random selection and returns it's choice.
+function computerPlay() {
     let options = ['rock', 'paper', 'scissors'];
-    let computerChoice = options[Math.floor(Math.random() * options.length)];
-    console.log('Computer choice: ' + computerChoice);
-
-    if (computerChoice === userChoice) {
-        alert('A tie! Nobody wins.');
-    }
-    
-    if (computerChoice === 'rock' && userChoice === 'paper') {
-        alert('You won! Paper covers rock.');
-    }
-
-    if (computerChoice === 'rock' && userChoice === 'scissors') {
-        alert('You have lost.. Rock breaks scissors.');
-    }
-
-    if (computerChoice === 'paper' && userChoice === 'rock') {
-        alert('You have lost.. Paper covers rock.');
-    }
-
-    if (computerChoice === 'paper' && userChoice === 'scissors') {
-        alert('Yeah you won! Scissors cuts paper!');
-    }
-
-    if (computerChoice === 'scissors' && userChoice === 'rock') {
-        alert('You won. Rock breaks scissors.');
-    }
-
-    if (computerChoice === 'scissors' && userChoice === 'paper') {
-        alert('Sorry. Scissors cuts paper. You have lost.');
-    } 
+    let computerSelection = options[Math.floor(Math.random() * options.length)];
+    return computerSelection;
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+   if (computerSelection === playerSelection) {
+       console.log('It\'s a tie! Nobody wins. Two rocks.');
+   } 
+   
+   if (computerSelection == 'rock' && playerSelection == 'paper') {
+       console.log('Oooh! You win! Paper covers rock.');
+   }
+   
+   if (computerSelection == 'rock' && playerSelection == 'scissors') {
+       console.log('Awh, sorry. You lose. Rock crushes scissors.');
+   }
+   
+   if (computerSelection == 'paper' && playerSelection == 'rock') {
+       console.log('Aw man, you lose. Paper covers rock.');
+   }
+   
+   if (computerSelection == 'paper' && playerSelection == 'scissors') {
+       console.log('Ooh! You win! Scissors definitely cuts paper.');
+   }
+   
+   if (computerSelection == 'scissors' && playerSelection == 'rock') {
+       console.log('Yay! You win. Rock crushes scissors.');
+   }
+   
+   if (computerSelection == 'scissors' && playerSelection == 'paper') {
+       console.log('Oh no! You lost. Scissors cuts paper.');
+   }
+   
+   if (playerSelection == ' ') {
+       console.log('Please choose between paper, rock or scissors.');
+   }
+}
+
+//The user is prompted to enter a selection and the text is converted to all lowercase
+let playerSelection = prompt('What do you choose? Rock, Paper or Scissors?').toLowerCase();
+
+//The result from computerPlay function is stored inside computerSelection variable
+let computerSelection = computerPlay();
+
+playRound();
 
